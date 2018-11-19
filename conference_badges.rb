044cf5db3417195c5d0e4cafd8d35edb67787183
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-19 22:07:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,8 @@ def assign_rooms (arrayofspeakers)
 end 
 
 def printer (attendees)
-  batch_badge_creator(attendees).each{|x| puts x};
-  assign_rooms(attendees).each{|x| puts x}
+  printbadgemessage = batch_badge_creator(attendees).each{|x| puts x};
+  printassignmessage = assign_rooms(attendees).each{|x| puts x};
+  puts printbadgemessage;
+  puts printbadgemessage
 end 
